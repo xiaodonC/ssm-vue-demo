b6f9752b6ab12e80cbd9c7e95a95b035b347fe97
chen-xd <chenxd@cn.ibm.com>
2017-05-09 11:04:54 +0800

    update upload layout

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,37 +1,35 @@
 <template>
 <el-col class="wrapper" :span="24">
-  <el-col :span="8" :offset="1" class="brandselect">
-    <span class="demonstration">Please select brand:</span>
-    <el-select v-model="value" clearable placeholder="请选择">
-        <el-option
-          v-for="item in options"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value">
-        </el-option>
-    </el-select>
+  <el-col class="uploadSelect" :span="24">
+    <el-col :span="8">
+      <span class="demonstration">Please select brand:</span>
+      <el-select v-model="value" clearable placeholder="select brand">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+      </el-select>
+    </el-col>
+    <el-col :span="8">
+      <span class="demonstration">Please select date:</span>
+      <el-date-picker
+        v-model="datevalue"
+        type="week"
+        format="yyyy-WW-week"
+        placeholder="select date">
+      </el-date-picker>
+    </el-col>
+    <el-col :span="8">
+      <el-upload
+        class="upload-demo"
+        action="">
+        <el-button size="normal" type="primary">点击上传</el-button>
+        <span slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</span>
+      </el-upload>
+    </el-col>
   </el-col>
-  <el-col :span="8" :offset="7" class="brandselect">
-    <span class="demonstration">Please select date:</span>
-    <el-date-picker
-      v-model="datevalue"
-      type="week"
-      format="yyyy-WW-week"
-      placeholder="select date">
-    </el-date-picker>
-  </el-col>
-  <el-col :span="6" :offset="9">
-    <el-upload
-      class="upload-demo"
-      drag
-      action="https://jsonplaceholder.typicode.com/posts/"
-      multiple>
-      <i class="el-icon-upload"></i>
-      <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-      <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
-    </el-upload>
-  </el-col>
-
 </el-col>
 </template>
 
@@ -42,18 +40,21 @@ export default {
       return {
         options: [{
           value: '选项1',
-          label: '黄金糕'
+          label: 'brand1'
         }, {
           value: '选项2',
-          label: '双皮奶'
+          label: 'brand2'
         }, {
           value: '选项3',
-          label: '蚵仔煎'
+          label: 'brand3'
         }],
         value: '',
         datevalue: ''
       }
-    }
+    },
+  methods: {
+
+  }
 }
 </script>
 
@@ -61,13 +62,12 @@ export default {
 .wrapper{
   margin-top: 10px;
   width: 100%;
-  height: 300px;
   border: 2px solid #20a0ff;
 }
-.brandselect{
-  padding-top: 10px;
-  padding-left: 10px;
-  margin-bottom: 20px;
+.uploadSelect{
+  padding: 10px;
 }
-
+.el-upload__tip{
+  margin-left: 10px;
+}
 </style>
